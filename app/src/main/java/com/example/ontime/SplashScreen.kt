2b054d124ba6cdc9445424aa8812f2dab6b9919a
BagasYudha <bayud.kematian@gmail.com>
2024-10-14 11:18:38 +0700

    Splash Screen and IC Launcher done

---
+++ app/src/main/java/com/example/ontime/SplashScreen.kt
@@ -0,0 +1,27 @@
+package com.example.ontime
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+
+        val splashEnd: Long = 1200
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+            finish()
+        }, splashEnd)
+
+    }
+}
