973739edf6321f3119ed4da58c9614335514a7bf
BagasYudha <bayud.kematian@gmail.com>
2024-10-20 15:13:21 +0700

    memperbaiki force close pada fragment dosen

--- app/src/main/java/com/example/ontime/setup/AppViewModel.kt
+++ app/src/main/java/com/example/ontime/setup/AppViewModel.kt
@@ -10,23 +10,15 @@ class AppViewModel(application: Application) : AndroidViewModel(application) {
 
     // Mengambil instance dari DAO untuk mengakses tabel di database
     private val dosenDao = AppDatabase.getDatabase(application).dosenDao()
-    private val matkulDao = AppDatabase.getDatabase(application).matkulDao()
 
     // LiveData berisi data dari database, digunakan agar UI dapat mengamati perubahan data secara otomatis
     val allDosen: LiveData<List<Dosen>> = dosenDao.getAllDosen()
-    val allMatkul: LiveData<List<MataKuliah>> = matkulDao.getAllMatkul()
 
     // Fungsi untuk memasukkan data yang diinput ke dalam tabel masing masing di database
     // Menggunakan coroutine viewModelScope untuk menjalankan proses ini di background thread
     fun insertDosen(dosen: Dosen) {
         viewModelScope.launch {
             dosenDao.insertDosen(dosen)  // Memasukkan data dosen ke database
-        }
-    }
-
-    fun inserMatkul(mataKuliah: MataKuliah) {
-        viewModelScope.launch {
-            matkulDao.insertMatkul(mataKuliah)
         }
     }
 
